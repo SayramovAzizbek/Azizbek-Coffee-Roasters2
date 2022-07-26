b201caab09b2a9c8ad4f71dd2bf148cdbd2de840
SayramovAzizbek <sayramovazizbek@gmail.com>
2022-07-26 18:38:52 +0500

    Add choose and collection section and add responsive

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let headerBtn = document.querySelector(".site-header-part__btn")
+
+let siteHeader = document.querySelector("#site-header")
+
+headerBtn.addEventListener("click", ()=> {
+siteHeader.classList.toggle("sitenav-on")
+})
